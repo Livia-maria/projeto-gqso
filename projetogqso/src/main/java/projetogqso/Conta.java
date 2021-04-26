35178ee066f114b2d8473c22b00524ed81ca2013
Livia Maria <lmsl2@aluno.ifal.edu.br>
2021-04-25 21:55:34 -0300

    Criação das classes

---
+++ projetogqso/src/main/java/projetogqso/Conta.java
@@ -0,0 +1,21 @@
+package projetogqso;
+
+public class Conta {
+    private double saldo =0;
+
+
+
+
+
+    
+public double getSaldo()
+{
+    return this.saldo;
+}
+
+public void setSaldo(double valor)
+{
+    this.saldo=valor;
+}
+    
+}
---
+++ projetogqso/src/test/java/projetogqso/TesteC.java
@@ -0,0 +1,16 @@
+package projetogqso;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TesteC {
+     Conta conta;
+
+
+     @Test
+     public void DepositoTeste()
+    {
+
+    }
+     
+    
+}
